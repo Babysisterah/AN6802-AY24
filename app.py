4a8cad8555d6b518495994b11625252b0de918b7
Babysisterah <luya0013@e.ntu.edu.sg>
2025-03-07 12:45:02 +0000

    1

--- app.py
+++ app.py
@@ -5,5 +5,22 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    user_name = request.form.get("q")    
+    return(render_template("main.html"))
+
+@app.route("/ethical_test",methods=["POST","GET"])
+def ethical_test(): 
+    return(render_template("ethical_test.html"))
+
+@app.route("/test_result",methods=["POST","GET"])
+def test_result(): 
+    anwser = request.form.get("answer") 
+    if anwser == "false":
+        return(render_template("pass.html"))
+    elif anwser == "true":
+        return(render_template("fail.html"))   
+
 if __name__ == "__main__":
     app.run()
